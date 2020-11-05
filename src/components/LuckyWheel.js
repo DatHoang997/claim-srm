e4ab5ff597139b51328519007b0016dbee0dcf81
Tien Nguyen <mrtienhp97@gmail.com>
2020-11-05 11:16:11 +0700

    fix share

--- src/components/LuckyWheel.js
+++ src/components/LuckyWheel.js
@@ -87,17 +87,14 @@ const LuckyWheel = (props) => {
   }
 
   const shareOnFB = () => {
-    FB.ui({
-      method: 'share',
-      href: `http://api-bounty.ezdefi.com//ref/${props.user.fb_id}`,
-    }, function(response){
-      if(response === undefined) {
-        openNotification('Chia sẻ liên kết thất bại. Vui lòng thử lại', 'error')
-      } else {
-        openNotification('Chia sẻ liên kết thành công. Bạn sẽ được thêm lượt quay sau khi bạn bè click vào liên hết mà bạn chia sẻ', 'info')
+    window.ReactNativeWebView && window.ReactNativeWebView.postMessage(JSON.stringify(
+      {
+        type: 'SHARE_BOUNTY',
+        payload: {
+          value: `Tham gia chương trình tặng 300aSRM và cơ hội trúng iPhone cực hot tại đây http://api-bounty.ezdefi.com/ref/${props.user.fb_id}`,
+        }
       }
-      console.log('share response', response);
-    });
+    ));
   }
 
   const onHideModal = () => {
